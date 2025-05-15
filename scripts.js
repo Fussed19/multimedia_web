f9451d1c5af2db2b054d6f8d5825054663cc4301
Fussed19 <diegopalencia19@gmail.com>
2025-05-15 20:52:32 +0000

    fiz audio

--- scripts.js
+++ scripts.js
@@ -6,15 +6,19 @@ function toggleMenu() {
 
 // Control de audio para el botón de mute
 const muteButton = document.getElementById('mute-button');
-const audio = new Audio('audio/cancionMain.mp3'); 
+const audio = new Audio('audio/cancionMain.mp3'); // Asegúrate de que esta ruta sea correcta
 
-audio.loop = true; 
-audio.play(); 
+audio.loop = true; // Reproduce en bucle
 
-let isMuted = false;
+// Inicia el audio solo después de una interacción del usuario
+let isMuted = true;
+audio.muted = true;
 
 muteButton.addEventListener('click', () => {
+  if (isMuted) {
+    audio.play(); // Reproduce el audio si está en mute
+  }
   isMuted = !isMuted;
-  audio.muted = isMuted; 
-  muteButton.querySelector('img').src = isMuted ? 'image/unmute_icon.png' : 'image/mute_icon.png'; // Cambia el icono
+  audio.muted = isMuted; // Activa o desactiva el mute
+  muteButton.querySelector('img').src = isMuted ? 'image/mute_icon.png' : 'image/unmute_icon.png'; // Cambia el icono
 });
