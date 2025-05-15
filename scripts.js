325f1e40d94f003101c8845d25e1366c54c2bd4d
Fussed19 <diegopalencia19@gmail.com>
2025-05-15 20:48:45 +0000

    Audio fix?

--- scripts.js
+++ scripts.js
@@ -1,4 +1,20 @@
+// Función para alternar el menú
 function toggleMenu() {
   const menu = document.getElementById('nav-menu');
   menu.classList.toggle('show');
 }
+
+// Control de audio para el botón de mute
+const muteButton = document.getElementById('mute-button');
+const audio = new Audio('audio/cancionMain.mp3'); 
+
+audio.loop = true; 
+audio.play(); 
+
+let isMuted = false;
+
+muteButton.addEventListener('click', () => {
+  isMuted = !isMuted;
+  audio.muted = isMuted; 
+  muteButton.querySelector('img').src = isMuted ? 'image/unmute_icon.png' : 'image/mute_icon.png'; // Cambia el icono
+});
